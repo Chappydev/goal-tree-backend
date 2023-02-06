f1627dccf98eac99474b4ea091a0e964a0295527
Chappydev <danjtchapman@gmail.com>
2023-02-05 21:19:14 -0700

    Update to only allow access/changes to the correct user

--- controllers/goals.js
+++ controllers/goals.js
@@ -1,29 +1,40 @@
 const Goal = require("../models/goal");
 const Node = require("../models/node");
+const { userFinder } = require("../utility/middleware");
 
 const goalsRouter = require("express").Router();
 
-goalsRouter.get("/:id", async (req, res) => {
+goalsRouter.get("/:id", userFinder, async (req, res) => {
   const goalData = await Goal.findById(req.params.id).populate("insertionNode");
   if (goalData) {
+    if (goalData.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ error: "users may only access their own goals" });
+    }
     res.json(goalData);
   } else {
     res.status(404).end();
   }
 });
 
-goalsRouter.post("/", async (req, res) => {
+goalsRouter.post("/", userFinder, async (req, res) => {
+  if (!req.user) {
+    return res.status(401).json({ error: "must be logged in to create goals" });
+  }
   if (!req?.body?.name) {
     res.status(400).json({ error: "Must include a name for the goal node" });
   }
   const { name } = req.body;
   const goalNode = new Node({
     name,
+    user: req.user._id,
   });
   const savedGoalNode = await goalNode.save();
 
   const goal = new Goal({
     insertionNode: savedGoalNode.id,
+    user: req.user._id,
   });
   const savedGoal = await goal.save();
 
--- controllers/nodes.js
+++ controllers/nodes.js
@@ -1,10 +1,12 @@
-const { nodes, generateId } = require("../utility/predbLeftovers");
 const Node = require("../models/node");
+const { userFinder } = require("../utility/middleware");
 
 const nodesRouter = require("express").Router();
 
-nodesRouter.get("/", async (req, res) => {
-  const nodes = await Node.find({}, null, { disableMiddlewares: true });
+nodesRouter.get("/", userFinder, async (req, res) => {
+  const nodes = await Node.find({ user: req.user._id }, null, {
+    disableMiddlewares: true,
+  });
   if (nodes) {
     res.json(nodes);
   } else {
@@ -12,32 +14,56 @@ nodesRouter.get("/", async (req, res) => {
   }
 });
 
-nodesRouter.get("/:id", async (req, res) => {
+nodesRouter.get("/:id", userFinder, async (req, res) => {
   const node = await Node.findById(req.params.id, null, {
     disableMiddlewares: true,
   });
   if (node) {
+    if (node.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ error: "users may only access their own goals" });
+    }
     res.json(node);
   } else {
     res.status(404).end();
   }
 });
 
-nodesRouter.put("/:id", async (req, res) => {
+nodesRouter.put("/:id", userFinder, async (req, res) => {
   const body = req.body;
   console.log(body);
-  const updatedNode = await Node.findByIdAndUpdate(req.params.id, body, {
-    new: true,
-  });
+  const updatedNode = await Node.findOneAndUpdate(
+    { _id: req.params.id, user: req.user._id },
+    body,
+    {
+      new: true,
+      disableMiddlewares: true,
+    }
+  );
+  if (!updatedNode) {
+    return res.status(404).end();
+  }
+
   res.json(updatedNode);
 });
 
-nodesRouter.post("/:id", async (req, res) => {
+nodesRouter.post("/:id", userFinder, async (req, res) => {
   const parentId = req.params.id;
 
-  const parentNode = await Node.findById(parentId);
+  if (!req.user) {
+    return res.status(401).json({ error: "must be logged in to create nodes" });
+  }
+
+  const parentNode = await Node.findById(parentId, null, {
+    disableMiddlewares: true,
+  });
   if (!parentNode) {
     return res.status(404).json({ error: "invalid parent id" });
+  } else if (parentNode.user.toString() !== req.user._id.toString()) {
+    return res
+      .status(401)
+      .json({ error: "users may only make changes to their own nodes" });
   }
 
   const body = req.body;
@@ -53,6 +79,7 @@ nodesRouter.post("/:id", async (req, res) => {
 
   const newNode = new Node({
     name,
+    user: req.user._id,
   });
 
   const savedNode = await newNode.save();
@@ -65,12 +92,22 @@ nodesRouter.post("/:id", async (req, res) => {
   res.status(201).json(savedNode);
 });
 
-nodesRouter.delete("/:id", async (req, res) => {
+nodesRouter.delete("/:id", userFinder, async (req, res) => {
   // TODO: remove children nodes of the specified node
 
-  const parentNode = await Node.findOne({
-    children: { $elemMatch: { $eq: req.params.id } },
-  });
+  const parentNode = await Node.findOne(
+    {
+      children: { $elemMatch: { $eq: req.params.id } },
+    },
+    null,
+    { disableMiddlewares: true }
+  );
+  if (parentNode.user.toString() !== req.user._id.toString()) {
+    return res
+      .status(401)
+      .json({ error: "users may only delete their own nodes" });
+  }
+
   if (parentNode) {
     const newChildren = [...parentNode.children];
     newChildren.splice(
@@ -81,7 +118,7 @@ nodesRouter.delete("/:id", async (req, res) => {
     await parentNode.save();
   }
 
-  await Node.findByIdAndDelete(req.params.id);
+  await Node.findByIdAndDelete(req.params.id, { disableMiddlewares: true });
 
   res.status(204).end();
 });
