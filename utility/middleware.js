3c3e2f45b55d51a10e4fea7c9798bbd7946287e7
Chappydev <danjtchapman@gmail.com>
2023-02-11 14:48:16 -0700

    Make usernames unique

--- utility/middleware.js
+++ utility/middleware.js
@@ -24,6 +24,8 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).json({ error: error.message });
   } else if (error.name === "JsonWebTokenError") {
     return res.status(400).json({ error: "token missing or invalid" });
+  } else if (error.name === "MongoServerError" && error.code === 11000) {
+    return res.status(400).json({ error: "that username is already taken" });
   }
 
   next(error);
