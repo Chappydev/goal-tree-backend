0e32b3bceb6af0679e634b92bee1e182a0cfb10a
Chappydev <danjtchapman@gmail.com>
2023-04-16 22:18:09 -0600

    Remove sensitive info from logs

--- utility/middleware.js
+++ utility/middleware.js
@@ -5,7 +5,11 @@ const logger = require("./logger");
 const requestLogger = (req, res, next) => {
   logger.info("Method:", req.method);
   logger.info("Path:  ", req.path);
-  logger.info("Body:  ", req.body);
+  if (req.body.password) {
+    logger.info("Body:  ", { ...req.body, password: "_" });
+  } else {
+    logger.info("Body:  ", req.body);
+  }
   logger.info("---");
   next();
 };
